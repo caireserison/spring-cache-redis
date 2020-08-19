ff47a3904342b0ec08d81d3234555a2150912a41
erison <erison@midway.com.br>
2020-08-19 14:15:17 -0300

    Tratando erro

---
+++ src/main/java/com/example/spring/cache/redis/config/CustomCacheErrorHandlerConfig.java
@@ -0,0 +1,32 @@
+package com.example.spring.cache.redis.config;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class CustomCacheErrorHandlerConfig implements CacheErrorHandler {@Override
+	public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+		System.out.printf(exception.getMessage());
+	}
+
+	@Override
+	public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+		System.out.printf(exception.getMessage());
+		
+	}
+
+	@Override
+	public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+		System.out.printf(exception.getMessage());
+		
+	}
+
+	@Override
+	public void handleCacheClearError(RuntimeException exception, Cache cache) {
+		System.out.printf(exception.getMessage());
+		
+	}
+
+}
---
+++ src/main/java/com/example/spring/cache/redis/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.example.spring.cache.redis.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig implements CachingConfigurer {
+
+	@Override
+	public CacheManager cacheManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CacheResolver cacheResolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public KeyGenerator keyGenerator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CacheErrorHandler errorHandler() {
+		return new CustomCacheErrorHandlerConfig();
+	}
+
+}
